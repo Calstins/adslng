d7e92b3c16ada4d73a2826f38c53cb0e67920fd9
Calstins <opulecalebtins@gmail.com>
2024-08-07 19:09:39 -0700

    edited service page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,5 @@
+import PageTitle from '@/components/common/PageTitle';
 import ContactUsSection from '@/components/sections/ContactUsSection';
-import HeroSection from '@/components/sections/HeroSection';
 import LetMakeThingsHappenSection from '@/components/sections/LetMakeThingsHappenSection';
 import OurWorkingProcessSection from '@/components/sections/OurWorkingProcessSection';
 import ServiceSection from '@/components/sections/ServiceSection';
@@ -9,8 +9,11 @@ const Services = () => {
   return (
     <section>
       <div className="mx-4 2xl:mx-[300px] md:mx-[100px] pt-[70px] flex flex-col gap-[100px]">
-        <HeroSection />
-        {/* <LogoGroupSection /> */}
+        <PageTitle
+          line1="Empowering Your Business"
+          line2="with Tailored Solutions"
+          content="ADSL provides expert services to enhance your business, including customized CRM and ERP solutions, comprehensive data network management, and bespoke applications. We also optimize network performance to ensure efficiency and functionality. Explore how our tailored solutions can drive your success.!"
+        />
         <ServiceSection />
         <LetMakeThingsHappenSection />
         {/* <CaseStudySection /> */}
--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -12,7 +12,7 @@ interface PageTitleProps {
 
 const PageTitle: React.FC<PageTitleProps> = ({ line1, line2, content }) => {
   return (
-    <div className="flex flex-col justify-between w-full 2xl:justify-around items-center text-center gap-5">
+    <div className="flex flex-col justify-between w-full 2xl:justify-around items-center text-center gap-5 mt-6">
       <p className="text-h1Mobile 2xl:text-4xl md:text-h2 text-black font-bold leading-tight">
         {line1} <br /> {line2}
       </p>
